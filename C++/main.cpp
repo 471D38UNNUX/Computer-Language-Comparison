32b4c1a1bbf26b84f62bada3aafea22b3dbf5f21
471D38UNNUX <aliwaskito123@gmail.com>
2025-08-11 12:47:00 +0700

    Update C++

--- C++/main.cpp
+++ C++/main.cpp
@@ -32,14 +32,14 @@ int                 main()
     auto elapsedTime    = static_cast<double>((counter.QuadPart / frequency.QuadPart) - time.tv_sec) + static_cast<double>(static_cast<int>((counter.QuadPart % frequency.QuadPart) * 1000000000 / frequency.QuadPart) - time.tv_nsec) / 1000000000.0;
     try
     {
-        auto Size   = file_size("main.exe");
-        cout        << "Total Cycles " << Cycles << endl;
-        cout        << "Time taken: " << static_cast<unsigned long long>(elapsedTime) / 3600 << " hours " << static_cast<unsigned long long>(elapsedTime) % 3600 / 60 << " minutes " << fixed << setprecision(6) << static_cast<double>(static_cast<unsigned long long>(elapsedTime) % 60) + elapsedTime - static_cast<double>(static_cast<unsigned long long>(elapsedTime)) << " seconds" << endl;
-        cout        << "Approx CPU frequency: " << static_cast<double>(Cycles) / elapsedTime / 1.0e9 << " GHz" << endl;
-        if          (static_cast<double>(Size) > gB)  cout << "File size: " << fixed << setprecision(3) << static_cast<double>(Size) / gB << " GB" << endl;
-        else        if (static_cast<double>(Size) > mB)  cout << "File size: " << fixed << setprecision(3) << static_cast<double>(Size) / mB << " MB" << endl;
-        else        if (static_cast<double>(Size) > kB)  cout << "File size: " << fixed << setprecision(3) << static_cast<double>(Size) / kB << " KB" << endl;
-        else        cout << "File size: " << Size << " bytes" << endl;
+        Size    = file_size("main.exe");
+        cout    << "Total Cycles " << Cycles << endl;
+        cout    << "Time taken: " << static_cast<unsigned long long>(elapsedTime) / 3600 << " hours " << static_cast<unsigned long long>(elapsedTime) % 3600 / 60 << " minutes " << fixed << setprecision(6) << static_cast<double>(static_cast<unsigned long long>(elapsedTime) % 60) + elapsedTime - static_cast<double>(static_cast<unsigned long long>(elapsedTime)) << " seconds" << endl;
+        cout    << "Approx CPU frequency: " << static_cast<double>(Cycles) / elapsedTime / 1.0e9 << " GHz" << endl;
+        if      (static_cast<double>(Size) > gB)  cout << "File size: " << fixed << setprecision(3) << static_cast<double>(Size) / gB << " GB" << endl;
+        else    if (static_cast<double>(Size) > mB)  cout << "File size: " << fixed << setprecision(3) << static_cast<double>(Size) / mB << " MB" << endl;
+        else    if (static_cast<double>(Size) > kB)  cout << "File size: " << fixed << setprecision(3) << static_cast<double>(Size) / kB << " KB" << endl;
+        else    cout << "File size: " << Size << " bytes" << endl;
         ExitProcess(0);
     }                   catch(filesystem_error &e) {ExitProcess(1);}
 }
