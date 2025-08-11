39fb53eebdf0a26badbcd2a8bc5086d87497838b
471D38UNNUX <aliwaskito123@gmail.com>
2025-08-11 13:19:16 +0700

    Update TypeScript

--- TypeScript/main.ts
+++ TypeScript/main.ts
@@ -3,7 +3,7 @@ import {dlopen} from "node:process"
 import {Buffer} from "node:buffer"
 import {statSync} from "node:fs"
 const module              = {exports: {}}
-dlopen(module, "./build/Release/main.node", 0)
+dlopen(module, "build/Release/main.node", 0)
 const FFI                 = module.exports as
 {
   QueryPerformanceFrequency(lpFrequency: Buffer): boolean
@@ -39,9 +39,9 @@ try
   console.log("Total Cycles " + Cycles.toString())
   console.log("Time taken: " + (BigInt(Math.trunc(elapsedTime)) / 3600n).toString() + " hours " + (BigInt(Math.trunc(elapsedTime)) % 3600n / 60n).toString() + " minutes " + (Number(BigInt(Math.trunc(elapsedTime)) % 60n) + elapsedTime - Math.trunc(elapsedTime)).toFixed(6) + " seconds")
   console.log("Approx CPU frequency: " + (Number(Cycles) / elapsedTime / 1.0e9).toFixed(6) + " GHz")
-  if        (Size > gB) console.log("File size: " + (Size / gB).toFixed(3) + " GB")
-  else      if (Size > mB) console.log("File size: " + (Size / mB).toFixed(3) + " MB")
-  else      if (Size > kB) console.log("File size: " + (Size / kB).toFixed(3) + " KB")
+  if        (Size >= gB) console.log("File size: " + (Size / gB).toFixed(3) + " GB")
+  else      if (Size >= mB) console.log("File size: " + (Size / mB).toFixed(3) + " MB")
+  else      if (Size >= kB) console.log("File size: " + (Size / kB).toFixed(3) + " KB")
   else      console.log("File size: " + Size.toString() + " bytes")
   FFI.ExitProcess(0)
 }                         catch (error) {FFI.ExitProcess(1)}
